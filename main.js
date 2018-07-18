b85e7f3185651836f7748121bcb2d006deb48a40
michellecarey <genovese28@yahoo.com>
2018-07-17 20:21:15 -0700

    updates

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ $(function() {
     //  which is the default form action
     event.preventDefault();
     // this variable points to the jQuery element
-    let $topText = $('#top');
-    // this variable is just a string
-    let topText = $topText.val();
+    //let $topText = $('#top');
+    // // this variable is just a string
+    let topText = $('#top').val();
+    let bottomText = $('#bottom').val();
+    let gif = $('#url').val();
 
-    let newParagraph = $(`<p>The top text is ${topText}</p>`);
-    // add a CSS class to it
-    newParagraph.addClass('thing');
-    // add it to the div id="container"
-    $('#container').append(newParagraph);
+    let newParagraph = $(`<p>${topText}</p>`);
+    let newImage = $(`<p>${gif} </p>`);
+
+    let bottomLine = $(`<p>${bottomText}</p>`);
+    // // add a CSS class to it
+    // newParagraph.addClass('thing');
+    // // add it to the div id="container"
+    $('#container')
+      .append(newParagraph)
+      .append(newImage)
+      .append(bottomLine);
   });
 });
